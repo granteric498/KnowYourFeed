524f76d60043d3812adcab2a879b69e8567e5107
granteric498 <granteric498@yahoo.com>
2018-09-24 19:55:35 -0500

    Working on Twitter API

--- public/javascript/app.1.js
+++ public/javascript/app.1.js
@@ -52,25 +52,25 @@ var tweetArr = [{
 
 // MAIN PROCESS CLICK EVENTS //
 
-$('#start').on('click',function(){
+$('#start').on('click', function () {
     // Removes the start button and instructions and loads the game
     $('#start').remove();
     $('#instruction').remove();
     game.loadQ();
 })
 
-$(document).on('click','.button1',function(e){
+$(document).on('click', '.button1', function (e) {
     // Passes the answer that the user clicks on to function
     game.clicked(e);
 })
 
-$(document).on('click','#reset',function(){
+$(document).on('click', '#reset', function () {
     // Resets the game without reloading the page
     $('#subcontainer').html("<h5 id='counter'> </h5>");
-    game.qIndex=0;
-    game.correct=0;
-    game.incorrect=0;
-    game.counter=15;
+    game.qIndex = 0;
+    game.correct = 0;
+    game.incorrect = 0;
+    game.counter = 15;
     game.loadQ();
 })
 
@@ -80,78 +80,78 @@ var game = {
     counter: 15,
     correct: 0,
     incorrect: 0,
-    countdown: function(){
+    countdown: function () {
         // game.counter;
         // $('#counter').html(game.counter);
-        if (game.counter<=0){
+        if (game.counter <= 0) {
             // console.log('Time Up!');
             // game.timesUp();
         }
     },
-    loadQ: function(){
-        timer = setInterval(game.countdown,1000);
+    loadQ: function () {
+        timer = setInterval(game.countdown, 1000);
         $('#subcontainer').html("<h5 id='counter'> </h5>");
-        $('#subcontainer').append("<h2>"+tweetArr[game.qIndex].twText+"</h2>");
-        for(var i=0;i<tweetArr.length;i++){
-            $('#subcontainer').append('<button class="button1" id="button-'+ i + '" data-name="'+tweetArr[i].authorID+'">'+'<img src='+tweetArr[i].twImage+'>'+tweetArr[i].authorID+'</button>'+'\n');
+        $('#subcontainer').append("<h2>" + tweetArr[game.qIndex].twText + "</h2>");
+        for (var i = 0; i < tweetArr.length; i++) {
+            $('#subcontainer').append('<button class="button1" id="button-' + i + '" data-name="' + tweetArr[i].authorID + '">' + '<img src=' + tweetArr[i].twImage + '>' + tweetArr[i].authorID + '</button>' + '\n');
 
         }
     },
-    nextQ: function(){
+    nextQ: function () {
         // game.counter = 15;
         // $('#counter').html(game.counter);
         game.qIndex++;
         game.loadQ();
     },
 
-    timesUp: function(){
+    timesUp: function () {
         // clearInterval(timer);
         game.incorrect++;
         $('#subcontainer').html('<h2> OUT OF TIME! </h2>');
-        if (game.qIndex==questionArr.length-1){
-            setTimeout(game.results,1*1000);
+        if (game.qIndex == questionArr.length - 1) {
+            setTimeout(game.results, 1 * 1000);
         } else {
-            setTimeout(game.nextQ,3*1000);
+            setTimeout(game.nextQ, 3 * 1000);
         }
     },
 
-    clicked: function(e){
+    clicked: function (e) {
         clearInterval(timer);
-        if($(e.target).data("name")==tweetArr[game.qIndex].authorID){
+        if ($(e.target).data("name") == tweetArr[game.qIndex].authorID) {
             game.rightAns();
         } else {
             game.wrongAns();
         }
     },
-    rightAns: function(){
+    rightAns: function () {
         // console.log("right");
         clearInterval(timer);
         game.correct++;
         $('#subcontainer').html('<h2> CORRECT!</h2>');
         // $('#subcontainer').append("<img src = "+questionArr[game.qIndex].image+"><p><a href='https://giphy.com/gifs/adultswim-3o7TKVH7nbfCVgzaBq'>via GIPHY</a></p>");
-        if (game.qIndex==tweetArr.length-1){
-            setTimeout(game.results,1*1000);
+        if (game.qIndex == tweetArr.length - 1) {
+            setTimeout(game.results, 1 * 1000);
         } else {
-            setTimeout(game.nextQ, 1*1000);
+            setTimeout(game.nextQ, 1 * 1000);
         }
     },
-    wrongAns: function(){
+    wrongAns: function () {
         // console.log("wrong");
         clearInterval(timer);
         game.incorrect++;
         $('#subcontainer').html('<h2> WRONG ANSWER!</h2>');
         // $('#subcontainer').append('<div style="width:50%;height:0;padding-bottom:30%;position:relative;"><iframe src="https://giphy.com/embed/Lfa0tFqDoHzd6" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/rick-and-morty-sanchez-smith-Lfa0tFqDoHzd6">via GIPHY</a></p>');
-        if (game.qIndex==tweetArr.length-1){
-            setTimeout(game.results,1*1000);
+        if (game.qIndex == tweetArr.length - 1) {
+            setTimeout(game.results, 1 * 1000);
         } else {
-            setTimeout(game.nextQ,1*1000);
+            setTimeout(game.nextQ, 1 * 1000);
         }
     },
-    results: function(){
+    results: function () {
         clearInterval(timer);
         $('#subcontainer').html('<h3> Game Completed! </h3>');
-        $('#subcontainer').append('<h3>Correct Answers: '+game.correct+' Wrong Answers: '+game.incorrect+' </h3><br>');
-        if (game.correct>game.incorrect) {
+        $('#subcontainer').append('<h3>Correct Answers: ' + game.correct + ' Wrong Answers: ' + game.incorrect + ' </h3><br>');
+        if (game.correct > game.incorrect) {
             $('#subcontainer').append('<h3>WINNER!!! </h3>');
             $('#subcontainer').append("<button class='button1' id='reset'>RESTART</button>");
             $('#subcontainer').append("<div style='width:50%;height:0;padding-bottom:25%;position:relative;'><iframe src='https://giphy.com/embed/3o7TKVH7nbfCVgzaBq' width='100%' height='100%' style='position:absolute' frameBorder='0' class='giphy-embed' allowFullScreen></iframe></div><p><a href='https://giphy.com/gifs/adultswim-3o7TKVH7nbfCVgzaBq'>via GIPHY</a></p>");
@@ -165,39 +165,37 @@ var game = {
 }
 
 // $("#test").on("click",() => {
-  
-    function getTweet(celebrity) {
-    var client = new Twitter({
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+
+function getTweet(celebrity) {
+    event.preventDefault();
+
+    var params = {
+        screen_name: celebrity,
+        count: 10
+    };
+
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i in tweets) {
+                // console.log(tweets[i].text);
+                if (tweets[i].favorite_count > 1000 && tweets[i].text.length > 100 && tweets[i].user.verified === true) {
+                    // console.log("True");
+                    console.log(tweets[i].text);
+                } else {
+                    console.log(`Favorites/tweets/verified issue`)
+                }
+                // console.log(tweets[i].favorite_count);
+            };
+        }
     });
+}
 
-    var params = {screen_name: celebrity, count: 10};
-        client.get('statuses/user_timeline', params, function(error, tweets, response) {
-            if (!error) {
-                for (var i in tweets) {   
-                    // console.log(tweets[i].text);
-                    if (tweets[i].favorite_count > 1000 && tweets[i].text.length > 100 && tweets[i].user.verified === true) {
-                        // console.log("True");
-                        console.log(tweets[i].text);
-                    } else {
-                        console.log(`Favorites/tweets/verified issue`)
-                    }
-                    // console.log(tweets[i].favorite_count);
-                };
-            }
-        });
-      
+$("#submit").on("click", getTweet($("#celebrityInput").val()));
 
+// Make sure we're able to link tweet to tweeter
+// tweets[i].user.name
 
+// pull database, push celebs into array 
+// run for loop that will that will run get tweet function 
 
-    }
-
-    getTweet()
-
-    //pull database, push celebs into array 
-    //run for loop that will that will run get tweet function 
-
-    //firebase for scoring
+// firebase for scoring
