99f800170e7eb36e455ed3c384fc446fb2de4227
granteric498 <granteric498@yahoo.com>
2018-09-25 03:24:37 -0500

    API key sometimes works for multiple calls.

--- routes/html.js
+++ routes/html.js
@@ -12,10 +12,6 @@ var client = new Twitter({
 
 var array = [];
 
-// function getTweets(celebrity) {
-
-// }
-
 // Routes
 // =============================================================
 module.exports = function (app) {
@@ -24,39 +20,86 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/index.1.html"));
     });
 
-    // app.post("/api/tweets", function (req, res) {
-    //     console.log(getTweets("JCrossover"));
-    //     array.push(getTweets("JCrossover"));
-    // });
-
     app.get("/api/tweets", function (req, res) {
-        // return res.json(array);
-        var params = {
-            screen_name: "KDTrey5",
-            // name: "Kevin Durant",
-            count: 10,
-        };
-    
-        client.get('statuses/user_timeline', params, function (error, tweets, response) {
-            if (!error) {
-                for (var i in tweets) {
-                    // This line includes making sure the celebrity is verified.
-                    // if (tweets[i].favorite_count > 1000 && tweets[i].text.length > 100 && tweets[i].user.verified === true) {
-                    if (tweets[i].favorite_count > 1000 && tweets[i].text.length > 100) {
-                        // console.log(tweets[i].text);
-                        // console.log(tweets[i].user.name);
-                        array.push(tweets[i].user.name, tweets[i].text);
+        function getTweets(celebrity) {
+            var params = {
+                screen_name: celebrity,
+                count: 10,
+            };
 
-                    } else {
-                        // Either the celebrity did not have over 1000 favorites or the tweet wasn't long enough.
-                        // console.log(`Favorites/tweets/verified issue`)
-                    }
+            client.get('statuses/user_timeline', params, function (error, tweets, response) {
+                if (!error) {
+                    for (var i in tweets) {
+                        // This line includes making sure the celebrity is verified.
+                        // if (tweets[i].favorite_count > 1000 && tweets[i].text.length > 100 && tweets[i].user.verified === true) {
+                        if (tweets[i].favorite_count > 5000 && tweets[i].text.length > 100) {
+                            array.push({
+                                "Name": tweets[i].user.name,
+                                "Twitter Handle": tweets[i].user.screen_name,
+                                "Tweet": tweets[i].text
+                            });
+                        };
+                    };
+                    // console.log(array);
+                    // res.json(array);
+                } else {
+                    console.log(error);
                 };
-                console.log(array);
-                return res.json(array);
-            } else {
-                console.log(error);
-            }
-        });
-    })
+            });
+        };
+
+        // Run the function for the celebrities we want:
+        // Needs to be the celebrity's twitter handle
+        // getTweets("IMKristenBell");
+        // getTweets("JonahHill");
+        // getTweets("AnnaKendrick47");
+        // getTweets("VancityReynolds");
+        // getTweets("LeoDiCaprio");
+        // getTweets("JLdaily");
+        // getTweets("TomCruise");
+        // getTweets("WillFerrel");
+        // getTweets("realjstatham");
+        // getTweets("TheRock");
+        // getTweets("meganfox");
+        // getTweets("AdamSandler");
+        // getTweets("vindiesel");
+        // getTweets("EyeOfJackieChan");
+        // getTweets("aplusk");
+        // getTweets("lindsaylohan");
+        // getTweets("Cristiano");
+        // getTweets("KingJames");
+        // getTweets("neymarjr");
+        // getTweets("kobebryant");
+        // getTweets("Ellen_Authentic");
+        // getTweets("KevinHart4real");
+        // getTweets("JimCarrey");
+        // getTweets("rickygervais");
+        // getTweets("rustyrockets");
+        // getTweets("billmaher");
+        // getTweets("katyperry");
+        // getTweets("justinbieber");
+        // getTweets("rihanna");
+        // getTweets("taylorswift13");
+        // getTweets("ArianaGrande");
+        // getTweets("jtimberlake");
+        // getTweets("ddlovato");
+        // getTweets("selenagomez");
+
+        // Displays all celebrities' objects on screen.
+
+        // function getAllTweets(callback) {
+        //     getTweets("Cristiano");
+        //     getTweets("KingJames");
+        //     getTweets("neymarjr");
+        //     callback();
+        // };
+
+        // // Run this function after the getAllTweets is complete
+        // function getData() {
+        //     res.send(array);
+        //     console.log(array);
+        // };
+
+        // getAllTweets(getData);
+    });
 };
