257509b54169238160468e26b40029d6a06f35e7
routhy46 <37312600+chum46@users.noreply.github.com>
2018-09-26 06:16:27 -0500

    reverting back to old UI to fix issues updated css to be mobile ready

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,3 +1,10 @@
+var api_avatars = [{
+    avatars: ["https://avatars.io/twitter/justinbieber/medium", "https://avatars.io/twitter/VancityReynolds/medium",
+    "https://avatars.io/twitter/mindykaling/medium", "https://avatars.io/twitter/kanyewest/medium", "https://avatars.io/twitter/donaldtrump/medium",
+    "https://avatars.io/twitter/JCrossover/medium", "https://avatars.io/twitter/kobebryant/medium", "https://avatars.io/twitter/steviesovak/medium"],
+    avname: ["Justin Bieber", "Ryan Reynolds", "Mindy Kaling", "Kanye West", "Dhruv", "Chum", "Grant", "Steven"]
+},
+];
 
 var tweetArr = [{
     twID: 1,
@@ -5,167 +12,177 @@ var tweetArr = [{
     twText: "I'd walk through a fire for my daughter...",
     twImage: "https://avatars.io/twitter/VancityReynolds/medium",
     authorID: "Ryan Reynolds",
-    answers: ["https://avatars.io/twitter/justinbieber/medium", "https://avatars.io/twitter/VancityReynolds/medium",
-        "https://avatars.io/twitter/mindykaling/medium", "https://avatars.io/twitter/kanyewest/medium", "https://avatars.io/twitter/donaldtrump/medium",
-        "https://avatars.io/twitter/JCrossover/medium", "https://avatars.io/twitter/kobebryant/medium", "https://avatars.io/twitter/steviesovak/medium"],
-    choices: ["Justin Bieber", "Ryan Reynolds", "Mindy Kaling", "Kanye West", "Dhruv", "Chum", "Grant", "Steven"]
+    answers: api_avatars[0],
+    choices: api_avatars[1]
 }, {
     twID: 2,
     twTopic: "random",
     twText: "Why is rhode island nor a road...",
     twImage: "https://avatars.io/twitter/justinbieber/medium",
     authorID: "Justin Bieber",
-    answers: ["https://avatars.io/twitter/justinbieber/medium", "https://avatars.io/twitter/VancityReynolds/medium",
-        "https://avatars.io/twitter/mindykaling/medium", "https://avatars.io/twitter/kanyewest/medium", "https://avatars.io/twitter/donaldtrump/medium",
-        "https://avatars.io/twitter/JCrossover/medium", "https://avatars.io/twitter/kobebryant/medium", "https://avatars.io/twitter/steviesovak/medium"],
-    choices: ["Justin Bieber", "Ryan Reynolds", "Mindy Kaling", "Kanye West", "Dhruv", "Chum", "Grant", "Steven"]
+    answers: api_avatars[0],
+    choices: api_avatars[1]
 }, {
     twID: 3,
     twTopic: "random",
     twText: "My head is too heavy",
     twImage: "https://avatars.io/twitter/mindykaling/medium",
     authorID: "Mindy Kaling",
-    answers: ["https://avatars.io/twitter/justinbieber/medium", "https://avatars.io/twitter/VancityReynolds/medium",
-        "https://avatars.io/twitter/mindykaling/medium", "https://avatars.io/twitter/kanyewest/medium", "https://avatars.io/twitter/donaldtrump/medium",
-        "https://avatars.io/twitter/JCrossover/medium", "https://avatars.io/twitter/kobebryant/medium", "https://avatars.io/twitter/steviesovak/medium"],
-    choices: ["Justin Bieber", "Ryan Reynolds", "Mindy Kaling", "Kanye West", "Dhruv", "Chum", "Grant", "Steven"]
+    answers: api_avatars[0],
+    choices: api_avatars[1]
 }, {
     twID: 4,
     twTopic: "random",
     twText: "BILL COSBY INNOCENT",
     twImage: "https://avatars.io/twitter/kanyewest/medium",
     authorID: "Kanye West",
-    answers: ["https://avatars.io/twitter/justinbieber/medium", "https://avatars.io/twitter/VancityReynolds/medium",
-        "https://avatars.io/twitter/mindykaling/medium", "https://avatars.io/twitter/kanyewest/medium", "https://avatars.io/twitter/donaldtrump/medium",
-        "https://avatars.io/twitter/JCrossover/medium", "https://avatars.io/twitter/kobebryant/medium", "https://avatars.io/twitter/steviesovak/medium"],
-    choices: ["Justin Bieber", "Ryan Reynolds", "Mindy Kaling", "Kanye West", "Dhruv", "Chum", "Grant", "Steven"]
+    answers: api_avatars[0],
+    choices: api_avatars[1]
 }, {
     twID: 5,
     twTopic: "random",
-    twText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+    twText: "Lorem Dipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
     twImage: "https://avatars.io/twitter/donaldtrump/medium",
     authorID: "Dhruv",
-    answers: ["https://avatars.io/twitter/justinbieber/medium", "https://avatars.io/twitter/VancityReynolds/medium",
-        "https://avatars.io/twitter/mindykaling/medium", "https://avatars.io/twitter/kanyewest/medium", "https://avatars.io/twitter/donaldtrump/medium",
-        "https://avatars.io/twitter/JCrossover/medium", "https://avatars.io/twitter/kobebryant/medium", "https://avatars.io/twitter/steviesovak/medium"],
-    choices: ["Justin Bieber", "Ryan Reynolds", "Mindy Kaling", "Kanye West", "Dhruv", "Chum", "Grant", "Steven"]
+    answers: api_avatars[0],
+    choices: api_avatars[1]
 }, {
     twID: 6,
     twTopic: "random",
-    twText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+    twText: "Lorem Cipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
     twImage: "https://avatars.io/twitter/JCrossover/medium",
     authorID: "Chum",
-    answers: ["https://avatars.io/twitter/justinbieber/medium", "https://avatars.io/twitter/VancityReynolds/medium",
-        "https://avatars.io/twitter/mindykaling/medium", "https://avatars.io/twitter/kanyewest/medium", "https://avatars.io/twitter/donaldtrump/medium",
-        "https://avatars.io/twitter/JCrossover/medium", "https://avatars.io/twitter/kobebryant/medium", "https://avatars.io/twitter/steviesovak/medium"],
-    choices: ["Justin Bieber", "Ryan Reynolds", "Mindy Kaling", "Kanye West", "Dhruv", "Chum", "Grant", "Steven"]
+    answers: api_avatars[0],
+    choices: api_avatars[1]
+}, {
 }, {
     twID: 7,
     twTopic: "random",
-    twText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+    twText: "Lorem Gipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
     twImage: "https://avatars.io/twitter/kobebryant/medium",
     authorID: "Grant",
-    answers: ["https://avatars.io/twitter/justinbieber/medium", "https://avatars.io/twitter/VancityReynolds/medium",
-        "https://avatars.io/twitter/mindykaling/medium", "https://avatars.io/twitter/kanyewest/medium", "https://avatars.io/twitter/donaldtrump/medium",
-        "https://avatars.io/twitter/JCrossover/medium", "https://avatars.io/twitter/kobebryant/medium", "https://avatars.io/twitter/steviesovak/medium"],
-    choices: ["Justin Bieber", "Ryan Reynolds", "Mindy Kaling", "Kanye West", "Dhruv", "Chum", "Grant", "Steven"]
+    answers: api_avatars[0],
+    choices: api_avatars[1]
 }, {
     twID: 8,
     twTopic: "random",
-    twText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+    twText: "Lorem Sipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
     twImage: "https://avatars.io/twitter/steviesovak/medium",
     authorID: "Steven",
-    answers: ["https://avatars.io/twitter/justinbieber/medium", "https://avatars.io/twitter/VancityReynolds/medium",
-        "https://avatars.io/twitter/mindykaling/medium", "https://avatars.io/twitter/kanyewest/medium", "https://avatars.io/twitter/donaldtrump/medium",
-        "https://avatars.io/twitter/JCrossover/medium", "https://avatars.io/twitter/kobebryant/medium", "https://avatars.io/twitter/steviesovak/medium"],
-    choices: ["Justin Bieber", "Ryan Reynolds", "Mindy Kaling", "Kanye West", "Dhruv", "Chum", "Grant", "Steven"]
+    answers: api_avatars[0],
+    choices: api_avatars[1]
 }];
 
 // MAIN PROCESS CLICK EVENTS //
 
-$('#start').on('click', function () {
-    // Removes the start button and center logo, loads the game
-    document.getElementById("game-title").innerHTML = "<img src='./images/KYF_logo_modified.png' height='167' width='225'>";
-    document.querySelector(".logo1").innerHTML = " ";
-    document.querySelector(".text2").innerHTML = " ";
-    document.getElementById("subcontainer").innerHTML = " ";
-    game.loadTweet();
-})
-
-$(document).on('click', '.button1', function (e) {
-    game.clicked(e);
-})
-
-$(document).on('click', '#reset', function () {
-    // Resets the game without reloading the page
+$('#start').on('click',function(){
+    // Removes the start button and instructions and loads the game
+    $('#start').remove();
+    $('#instruction').remove();
     game.loadQ();
 })
 
+$(document).on('click','.button1',function(e){
+    // Passes the answer that the user clicks on to function
+    game.clicked(e);
+})
+
+$(document).on('click','#reset',function(){
+    // Resets the game without reloading the page
+    $('#subcontainer').html("<h5 id='counter'> </h5>");
+    game.qIndex=0;
+    game.correct=0;
+    game.incorrect=0;
+    game.counter=15;
+    game.loadQ();
+})
+
+// OBJECT CONTAINING THE GAME FUNCTIONS //
 var game = {
-    // Properties
-    tweetArr: tweetArr,
-    currentTweet: 0,
+    qIndex: 0,
+    counter: 15,
     correct: 0,
     incorrect: 0,
-    // Methods
-    loadTweet: function () {
-        $("#subcontainer").html("<h2>" + tweetArr[game.currentTweet].twText + "</h2>");
-        for (var i = 0; i < tweetArr[game.currentTweet].answers.length; i++) {
-            // Add a randomizer here for avatars
-            // Sets the data-name attribute as the correct author of the tweet
-            $('#subcontainer').append('<button class="button1" id="button-' + i + '" data-name="'
-                + tweetArr[game.currentTweet].choices[i] + '">' + '<img src=' + tweetArr[game.currentTweet].answers[i] + '>' +
-                tweetArr[game.currentTweet].choices[i] + '</button>' + '\n');
+    countdown: function(){
+        // game.counter;
+        // $('#counter').html(game.counter);
+        if (game.counter<=0){
+            // console.log('Time Up!');
+            // game.timesUp();
         }
     },
-    nextTweet: function () {
-        game.currentTweet++;
-        game.loadTweet();
+    loadQ: function(){
+        timer = setInterval(game.countdown,1000);
+        $('#subcontainer').html("<h5 id='counter'> </h5>");
+        $('#subcontainer').append("<h2>"+tweetArr[game.qIndex].twText+"</h2>");
+        for(var i=0;i<tweetArr.length;i++){
+            $('#subcontainer').append('<button class="button1" id="button-'+ i + '" data-name="'+tweetArr[i].authorID+'">'+'<img src='+tweetArr[i].twImage+'>'+tweetArr[i].authorID+'</button>'+'\n');
+
+        }
     },
-    clicked: function (e) {
-        console.log(e);
-        if ($(e.target).data("name") == tweetArr[game.currentTweet].authorID) {
+    nextQ: function(){
+        // game.counter = 15;
+        // $('#counter').html(game.counter);
+        game.qIndex++;
+        game.loadQ();
+    },
+
+    timesUp: function(){
+        // clearInterval(timer);
+        game.incorrect++;
+        $('#subcontainer').html('<h2> OUT OF TIME! </h2>');
+        if (game.qIndex==questionArr.length-1){
+            setTimeout(game.results,1*1000);
+        } else {
+            setTimeout(game.nextQ,3*1000);
+        }
+    },
+
+    clicked: function(e){
+        clearInterval(timer);
+        if($(e.target).data("name")==tweetArr[game.qIndex].authorID){
             game.rightAns();
         } else {
             game.wrongAns();
         }
     },
-    rightAns: function () {
-        console.log("Right");
+    rightAns: function(){
+        // console.log("right");
+        clearInterval(timer);
         game.correct++;
-        $('#subcontainer').html('<h2>CORRECT!</h2>');
-        if (game.currentTweet == tweetArr.length - 1) {
-            setTimeout(game.results, 1 * 1000);
+        $('#subcontainer').html('<h2> CORRECT!</h2>');
+        // $('#subcontainer').append("<img src = "+questionArr[game.qIndex].image+"><p><a href='https://giphy.com/gifs/adultswim-3o7TKVH7nbfCVgzaBq'>via GIPHY</a></p>");
+        if (game.qIndex==tweetArr.length-1){
+            setTimeout(game.results,1*1000);
         } else {
-            setTimeout(game.nextTweet, 1 * 1000);
+            setTimeout(game.nextQ, 1*1000);
         }
     },
-    wrongAns: function () {
-        console.log("Wrong");
+    wrongAns: function(){
+        // console.log("wrong");
+        clearInterval(timer);
         game.incorrect++;
-        $('#subcontainer').html('<h2> WRONG ANSWER! </h2>');
-        if (game.currentTweet == tweetArr.length - 1) {
-            setTimeout(game.results, 1 * 1000);
+        $('#subcontainer').html('<h2> WRONG ANSWER!</h2>');
+        // $('#subcontainer').append('<div style="width:50%;height:0;padding-bottom:30%;position:relative;"><iframe src="https://giphy.com/embed/Lfa0tFqDoHzd6" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/rick-and-morty-sanchez-smith-Lfa0tFqDoHzd6">via GIPHY</a></p>');
+        if (game.qIndex==tweetArr.length-1){
+            setTimeout(game.results,1*1000);
         } else {
-            setTimeout(game.nextTweet, 1 * 1000);
+            setTimeout(game.nextQ,1*1000);
         }
     },
-    results: function () {
+    results: function(){
+        clearInterval(timer);
         $('#subcontainer').html('<h3> Game Completed! </h3>');
-        $('#subcontainer').append('<h3>Correct Answers: ' + game.correct + ' Wrong Answers: ' + game.incorrect + ' </h3><br>');
-        if (game.correct > game.incorrect) {
+        $('#subcontainer').append('<h3>Correct Answers: '+game.correct+' Wrong Answers: '+game.incorrect+' </h3><br>');
+        if (game.correct>game.incorrect) {
             $('#subcontainer').append('<h3>WINNER!!! </h3>');
             $('#subcontainer').append("<button class='button1' id='reset'>RESTART</button>");
             $('#subcontainer').append("<div style='width:50%;height:0;padding-bottom:25%;position:relative;'><iframe src='https://giphy.com/embed/3o7TKVH7nbfCVgzaBq' width='100%' height='100%' style='position:absolute' frameBorder='0' class='giphy-embed' allowFullScreen></iframe></div><p><a href='https://giphy.com/gifs/adultswim-3o7TKVH7nbfCVgzaBq'>via GIPHY</a></p>");
         } else {
             $('#subcontainer').append('<h3>YOU LOSE... </h3><br><h4>Try Again.</h4>');
             $('#subcontainer').append("<button class='button2' id='reset'>RESTART</button>");
+            // $('#subcontainer').append("<div style='width:50%;height:0;padding-bottom:25%;position:relative;'><iframe src='https://giphy.com/embed/3o7TKVH7nbfCVgzaBq' width='100%' height='100%' style='position:absolute' frameBorder='0' class='giphy-embed' allowFullScreen></iframe></div><p><a href='https://giphy.com/gifs/adultswim-3o7TKVH7nbfCVgzaBq'>via GIPHY</a></p>");
         }
-    },
-    reset: function () {
-        game.correct = 0;
-        game.incorrect = 0;
-        game.currentTweet = 0;
-        game.loadTweet();
+        // $('#subcontainer').append("<button class='button1' id='reset'>RESTART</button>");
     }
 }
